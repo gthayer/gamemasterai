dffbc583e860ce030378cb5819233a1d01b08c5c
Gary Thayer <gary.thayer@10up.com>
2023-03-04 10:29:30 -0500

    remove some providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,10 @@
 import NextAuth from 'next-auth'
-import FacebookProvider from 'next-auth/providers/facebook'
-import GoogleProvider from 'next-auth/providers/google'
-import GitHubProvider from 'next-auth/providers/github'
-import RedditProvider from 'next-auth/providers/reddit'
+// import FacebookProvider from 'next-auth/providers/facebook'
+// import GoogleProvider from 'next-auth/providers/google'
+// import RedditProvider from 'next-auth/providers/reddit'
 import PatreonProvider from 'next-auth/providers/patreon'
+import GitHubProvider from 'next-auth/providers/github'
+
 
 export default NextAuth({
   providers: [
@@ -11,21 +12,21 @@ export default NextAuth({
       clientId: process.env.PATREON_ID,
       clientSecret: process.env.PATREON_SECRET,
     }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET
+    // }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET
+    // }),
+    // RedditProvider({
+    //   clientId: process.env.REDDIT_CLIENT_ID,
+    //   clientSecret: process.env.REDDIT_CLIENT_SECRET
+    // }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
-    }),
-    RedditProvider({
-      clientId: process.env.REDDIT_CLIENT_ID,
-      clientSecret: process.env.REDDIT_CLIENT_SECRET
     })
   ]
 })
