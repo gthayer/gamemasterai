85e32d47acf2e05597f3c79e25e3dea94c802b6d
Gary Thayer <gary.thayer@10up.com>
2023-03-10 15:16:00 -0500

    add reddit provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,29 +1,24 @@
 import NextAuth from 'next-auth'
 // import FacebookProvider from 'next-auth/providers/facebook'
 // import GoogleProvider from 'next-auth/providers/google'
-// import RedditProvider from 'next-auth/providers/reddit'
-import PatreonProvider from 'next-auth/providers/patreon'
+import RedditProvider from 'next-auth/providers/reddit'
 import GitHubProvider from 'next-auth/providers/github'
 
 
 export default NextAuth({
   providers: [
-    PatreonProvider({
-      clientId: process.env.PATREON_ID,
-      clientSecret: process.env.PATREON_SECRET,
-    }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_ID,
-    //   clientSecret: process.env.FACEBOOK_SECRET
-    // }),
     // GoogleProvider({
     //   clientId: process.env.GOOGLE_ID,
     //   clientSecret: process.env.GOOGLE_SECRET
     // }),
-    // RedditProvider({
-    //   clientId: process.env.REDDIT_CLIENT_ID,
-    //   clientSecret: process.env.REDDIT_CLIENT_SECRET
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET
     // }),
+    RedditProvider({
+      clientId: process.env.REDDIT_CLIENT_ID,
+      clientSecret: process.env.REDDIT_CLIENT_SECRET
+    }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
