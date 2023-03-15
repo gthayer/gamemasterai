d4ac50f37fedf6cfd46978128b91e03ca4d145fa
Gary Thayer <gary.thayer@10up.com>
2023-03-15 16:49:41 -0400

    center login button

--- components/auth/login.js
+++ components/auth/login.js
@@ -3,7 +3,7 @@ import { useSession, signIn } from "next-auth/react"
 export default function Login() {
 	const { data: session } = useSession();
 
-	let className = "bg-orange font-bold block w-5/12 align-center mt-2 py-2 px-4 focus:outline-none focus:shadow-outline";
+	let className = "bg-orange font-bold block w-5/12 align-center mt-2 m-auto py-2 px-4 focus:outline-none focus:shadow-outline";
 
 	return (
 		<button onClick={signIn} className={className}>Sign In</button>
