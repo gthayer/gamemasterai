30ce677cd8d87fbea76811f8929048b9e527cad3
Gary Thayer <gary.thayer@10up.com>
2023-03-15 17:39:04 -0400

    rating layout

--- components/ratings/index.js
+++ components/ratings/index.js
@@ -1,13 +1,15 @@
 import Image from 'next/image';
+import styles from './ratings.module.css';
 
 export default function Ratings() {
 
 	return (
-		<div className="float-right w-6/12 text-sm mb-4 text-center">
+		<div className="ratings float-right w-6/12 text-sm mb-4 text-center">
+			<div className={styles.ratings}>
 				<p>Is this what you were looking for?</p>
 				<Image
 					priority
-					className="fill-tan inline-block"
+					className="fill-tan inline-block hover:bg-tan active:bg-tan focus:outline-none focus:ring focus:ring-tan"
 					src="/images/happy-face.svg"
 					height={28}
 					width={28}
@@ -15,12 +17,13 @@ export default function Ratings() {
 				/>
 				<Image
 					priority
-					className="fill-tan inline-block ml-2"
+					className="fill-tan inline-block ml-4"
 					src="/images/sad-face.svg"
 					height={28}
 					width={28}
 					alt="Join us on Reddit"
 				/>
+			</div>
 		</div>
 	)
 }
