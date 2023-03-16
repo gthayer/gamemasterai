7afea8daa4eb62205efb983c644b4d6527abea92
Gary Thayer <gary.thayer@10up.com>
2023-03-16 17:36:45 -0400

    rating interactive actions

--- components/rating/index.js
+++ components/rating/index.js
@@ -4,11 +4,18 @@ import { useState } from 'react';
 
 const endpoint = '/api/rating';
 
-export default function Rating(statBlockId) {
+export default function Rating({statBlock}) {
 
 	const [isLoading, setIsLoading] = useState(false);
+	const [rating, setRating] = useState('');
 
-	async function rateBlock(rating) {
+	async function rateBlock(r) {
+		
+		// Unset the value;
+		if (rating === r) {
+			r = '';
+		}
+
 		setIsLoading(true);
 		const response = await fetch(endpoint, {
 			method: "POST",
@@ -16,21 +23,22 @@ export default function Rating(statBlockId) {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({
-				statBlockId: statBlockId.statBlockId,
+				id: statBlock.id,
 				rating: rating
 			}),
 		});
+		setRating(r);
 		const resp = await response.json();
 		setIsLoading(false);
 	}
 
 	return (
-		<div className="rating float-right w-6/12 text-sm mb-4 text-center">
+		<div className='rating float-right w-6/12 text-sm mb-4 text-center'>
 			<div className={styles.rating}>
 				<p>Is this what you were looking for?</p>
 				<Image
 					priority
-					className="fill-tan inline-block hover:bg-tan active:bg-tan focus:outline-none focus:ring focus:ring-tan"
+					className={`${rating === 'like' ? `${styles.like}` : ''} fill-tan inline-block`}
 					src="/images/happy-face.svg"
 					height={28}
 					width={28}
@@ -39,7 +47,7 @@ export default function Rating(statBlockId) {
 				/>
 				<Image
 					priority
-					className="fill-tan inline-block ml-4"
+					className={`${rating === 'dislike' ? `${styles.dislike}` : ''} fill-tan inline-block ml-4`}
 					src="/images/sad-face.svg"
 					height={28}
 					width={28}
